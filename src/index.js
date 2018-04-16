60998d15fc598d7630054cf8d2edfcfb832d8fdf
pawel misiak <pawelmisiak@live.com>
2018-04-16 17:10:39 -0400

    finish video_list and video_list_item. Also selecting feature now works.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar';
-import VideoList from './components/video_list'
+import VideoList from './components/video_list';
+import VideoDetail from './components/video_detail';
 
 //youtube api key
 const API_KEY = 'AIzaSyCxWaK46Teulqbb3-sFQRMhrk1mpABoS5w';
@@ -12,18 +13,29 @@ class App extends Component  {
   constructor(props) {
     super(props);
 
-    this.state = { videos: [] };
+    this.state = {
+      videos: [],
+      selectedVideo: null,
+    };
 
     // fat arrow is the same as this.setState({videos: videos})
-    YTSearch({ key: API_KEY, term: 'surfboards' }, videos => {this.setState({ videos });
-    })
+    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+      this.setState({
+      videos: videos,
+      selectedVideo: videos[0],
+     });
+    });
   }
+
   render(){
     return (
       <div>
         <SearchBar />
+        <VideoDetail video={this.state.selectedVideo} />
         {/* passing props to the child */}
-        <VideoList videos={this.state.videos} />
+        <VideoList
+          onVideoSelect={selectedVideo => this.setState({ selectedVideo })}
+          videos={this.state.videos} />
       </div>
     );
   }
