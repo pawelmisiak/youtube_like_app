1fab3f9726409de558e3573fefa77a096d6893f8
pawel misiak <pawelmisiak@live.com>
2018-04-16 14:54:37 -0400

    successfully used youtube api to search for video and return array with info for 5 videos. Now work on Video_list module

--- src/index.js
+++ src/index.js
@@ -1,9 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
+import SearchBar from './components/search_bar';
+import VideoList from './components/video_list'
+
+//youtube api key
+const API_KEY = 'AIzaSyCxWaK46Teulqbb3-sFQRMhrk1mpABoS5w';
 
 //create a new component.
-const App = () => {
-  return <div>Hi!</div>;
+class App extends Component  {
+  constructor(props) {
+    super(props);
+
+    this.state = { videos: [] };
+
+    // fat arrow is the same as this.setState({videos: videos})
+    YTSearch({ key: API_KEY, term: 'surfboards' }, videos => {this.setState({ videos });
+    })
+  }
+  render(){
+    return (
+      <div>
+        <SearchBar />
+        {/* passing props to the child */}
+        <VideoList videos={this.state.videos} />
+      </div>
+    );
+  }
 }
 
 //take this component and generate html
