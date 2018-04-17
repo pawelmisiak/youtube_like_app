87c46ea428d56176f07d8db95934bf5a7d28fa8c
pawel misiak <pawelmisiak@live.com>
2018-04-17 00:00:56 -0400

    finish work on updating video list when different titles are typed into the search box. Now moving on to work on throttling the search

--- src/index.js
+++ src/index.js
@@ -17,20 +17,23 @@ class App extends Component  {
       videos: [],
       selectedVideo: null,
     };
+    this.videoSearch('surfboards');
+  }
 
+  videoSearch(term) {
     // fat arrow is the same as this.setState({videos: videos})
-    YTSearch({ key: API_KEY, term: 'surfboards' }, (videos) => {
+    YTSearch({ key: API_KEY, term: term }, (videos) => {
       this.setState({
-      videos: videos,
-      selectedVideo: videos[0],
-     });
+        videos: videos,
+        selectedVideo: videos[0],
+      });
     });
   }
 
   render(){
     return (
       <div>
-        <SearchBar />
+        <SearchBar onSearchTermChange={term => this.videoSearch(term)} />
         <VideoDetail video={this.state.selectedVideo} />
         {/* passing props to the child */}
         <VideoList
